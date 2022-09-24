49f3846c83ac00c3be497e708e800df2a5faee1c
DatSung-Dev <datsung.dev@gmail.com>
2022-09-24 21:08:50 +0700

    update

--- Workshop_1/Program_4.c
+++ Workshop_1/Program_4.c
@@ -7,11 +7,15 @@ int main() {
     {
         printf("Enter number A and number B: ");
         scanf("%d %d", &Num_A ,&Num_B);
+
         Num_A = Num_A + Num_B;
         Num_B = Num_A - Num_B;
         Num_A = Num_A - Num_B;
+        
         printf("Num_A = %d, Num_B = %d\n", Num_A, Num_B);
+
         printf("\n");
+
     } while (Num_A != 0 && Num_B != 0);
     
 }
