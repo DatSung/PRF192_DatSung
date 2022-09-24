776072381153c78d5bdfc473de4442bd5de5a80f
DatSung-Dev <datsung.dev@gmail.com>
2022-09-24 20:16:17 +0700

    update

--- Workshop_1/Program_1.c
+++ Workshop_1/Program_1.c
@@ -2,31 +2,41 @@
 #include <math.h>
 
 int main() {
-    int dependent, tF = 0, tI = 0, income, income_Tax = 0;
-    const int PA = 9000000, PD = 3600000;
+    double dependent, tF = 0, tI = 0, income, income_Tax = 0;
+    const double PA = 9000000, PD = 3600000;
 
     printf("Enter your income value: ");
-    scanf("%d", &income);
+    scanf("%lf", &income);
     printf("Enter your number of dependent: ");
-    scanf("%d", &dependent);
+    scanf("%lf", &dependent);
 
     tF = 12 * (PA + (dependent * PD));
-    tI = sqrt(income - tF);
+    tI = fabs(income - tF);
 
     if (tI <= 0)
-    {
+    {   
+        printf("Your tax-free income is: %lf\n", tF);
+        printf("Your taxable income is: %lf\n", tI);
         printf("Your income tax is 0\n");
     } else if (tI <= 5000000)
-    {
+    {   
+        printf("Your tax-free income is: %lf\n", tF);
+        printf("Your taxable income is: %lf\n", tI);
         printf("Your income tax is: %.2f\n", tI * 0.05);
     } else if (tI <= 10000000)
-    {
-        printf("Your income tax is: %.2f\n", tI * 0.1);
+    {   
+        printf("Your tax-free income is: %lf\n", tF);
+        printf("Your taxable income is: %lf\n", tI);
+        printf("Your income tax is: %.2lf\n", tI * 0.1);
     } else if (tI <= 18000000)
-    {
-        printf("Your income tax is: %.2f\n", tI * 0.15);
+    {   
+        printf("Your tax-free income is: %lf\n", tF);
+        printf("Your taxable income is: %lf\n", tI);
+        printf("Your income tax is: %.2lf\n", tI * 0.15);
     } else
-    {
-        printf("Your income tax is: %.2f\n", tI * 0.2);
+    {   
+        printf("Your tax-free income is: %lf\n", tF);
+        printf("Your taxable income is: %lf\n", tI);
+        printf("Your income tax is: %.2lf\n", tI * 0.2);
     }
 }
