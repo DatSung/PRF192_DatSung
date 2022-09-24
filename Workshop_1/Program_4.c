9c63c61596213c52845f680f65eea0a63f88a9b9
DatSung-Dev <datsung.dev@gmail.com>
2022-09-24 21:05:39 +0700

    update

---
+++ Workshop_1/Program_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int Num_A, Num_B;
+
+    do
+    {
+        printf("Enter number A and number B: ");
+        scanf("%d %d", &Num_A ,&Num_B);
+        Num_A = Num_A + Num_B;
+        Num_B = Num_A - Num_B;
+        Num_A = Num_A - Num_B;
+        printf("Num_A = %d, Num_B = %d\n", Num_A, Num_B);
+        printf("\n");
+    } while (Num_A != 0 && Num_B != 0);
+    
+}
