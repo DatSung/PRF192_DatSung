4b40da9030eaad060182976ddb1a3ba7a35230ed
DatSung-Dev <109453487+DatSung@users.noreply.github.com>
2022-10-13 13:35:57 +0700

    update

---
+++ Workshop_6/Program_3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    int stringNumbers[255];
+    int numbers_count, sum = 0;
+
+    printf("Please enter a number to get string: ");
+    scanf("%d", &numbers_count);
+
+    for (i = 0; i < numbers_count; i++)
+    {
+        scanf("%d", &stringNumbers[i]);
+    }
+
+    for (i = 0; i < numbers_count; i++)
+    {
+        sum += stringNumbers[i];
+    }
+    
+    printf("The sum of string is: %d\n", sum);
+    printf("The last element in string is: %d", stringNumbers[numbers_count - 1]);
+}
