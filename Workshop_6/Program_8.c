c52332e9d39cea3e9dc487f5f5d1e9e93ef644bc
DatSung-Dev <109453487+DatSung@users.noreply.github.com>
2022-10-20 13:27:44 +0700

    update

---
+++ Workshop_6/Program_8.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int storage[255];
+
+void stringInput();
+void stringSort();
+void stringPrint();
+
+int main() {    
+
+    int digit_string;
+    printf("Please enter digit of string: ");
+    scanf("%d", &digit_string);
+    printf("Please enter string of number: ");
+    stringInput(digit_string);
+    stringSort(digit_string);
+    printf("String after soft is: ");
+    stringPrint(digit_string);
+
+}
+
+void stringInput(int digit_string) {
+
+    int i;
+
+    for (i = 0; i < digit_string; i++)
+    {
+        scanf("%d", &storage[i]);
+    }
+}
+
+void stringSort(int digit_string) {
+
+    int i, j, trade;
+
+    for(i = 0; i < digit_string - 1; i++)
+    {
+        for( j = i + 1; j < digit_string; j++)
+        {
+            if(storage[i] > storage[j])
+            {
+                trade = storage[i];
+                storage[i] = storage[j];
+                storage[j] = trade;        
+            }
+        }
+    }   
+}
+
+void stringPrint(int digit_string) {
+    
+    int i;
+
+    for (i = 0; i < digit_string; i++)
+    {
+        printf("%d ", storage[i]);
+    }  
+}
---
+++ demo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main {
+    int arr[] = {3,6,5,9,7,1,8};
+
+    for (int i = 0; i < 7; i++)
+    {
+        
+    }
+    
+
+}
