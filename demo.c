4f300c62be7b2212fafd7f66dd6a56f238f2928a
DatSung-Dev <109453487+DatSung@users.noreply.github.com>
2022-10-21 14:59:23 +0700

    update

--- demo.c
+++ demo.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 
-int main {
+int main() {
     int arr[] = {3,6,5,9,7,1,8};
 
-    for (int i = 0; i < 7; i++)
+    int i, j, trade;
+
+    for(i = 0; i < 7 - 1; i = i + 2)
     {
-        
+        for( j = i + 2; j < 7; j = j + 2)
+        {
+            if(arr[i] < arr[j])
+            {
+                trade = arr[i];
+                arr[i] = arr[j];
+                arr[j] = trade;        
+            }
+        }
+    }   
+
+    for (i = 0; i < 7; i++)
+    {
+        printf("%4d", arr[i]);
     }
     
-
 }
