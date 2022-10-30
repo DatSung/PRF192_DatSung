59dc0dd504f9756c7391b0f835dd42dc3a12c3ea
DatSung-Dev <109453487+DatSung@users.noreply.github.com>
2022-10-30 21:05:39 +0700

    update

---
+++ Workshop_7/Ex_3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void palindromesChecker();
+
+int main() {
+    int num_enter;
+
+    scanf("%d", &num_enter);
+    palindromesChecker(num_enter);
+}
+
+void palindromesChecker(int num_enter) {
+
+    int num_enter_clone, i, ram, sum;
+    
+    sum = 0;
+    num_enter_clone = num_enter;
+
+    while (num_enter_clone > 0)
+    {
+        ram = num_enter_clone % 10;
+        sum = (sum * 10) + ram;
+        num_enter_clone = num_enter_clone  / 10;
+    }   
+
+    if (num_enter == sum)
+    {
+        printf("%d is a palindrome number.", num_enter);
+    } 
+    else
+    {
+        printf("%d is not a palindrome number.", num_enter);
+    }     
+}
