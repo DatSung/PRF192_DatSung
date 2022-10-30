bca0fb912c1f30d31b95dc1082dc426ea8389fc4
DatSung-Dev <109453487+DatSung@users.noreply.github.com>
2022-10-30 22:14:12 +0700

    update

--- Workshop_7/Ex_4.c
+++ Workshop_7/Ex_4.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h> 
 
 int main() {
+
+    int *ptr, num_enter, i, square;
+    square = 1;
+
+    printf("Please enter amount of number: ");
+    scanf("%d", &num_enter);
+
+    ptr = (int *) malloc(num_enter * sizeof(int));
+
+    for (i = 0; i < num_enter; i++)
+    {
+        scanf("%d", ptr + i);
+    }
     
+    for (i = 0; i < num_enter; i++)
+    {
+        if (*(ptr + i) % 2 == 0)
+        {
+            square = square * *(ptr + i);
+        }
+    }
+
+    printf("%d", square);
 }
