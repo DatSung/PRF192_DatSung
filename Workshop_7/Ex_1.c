9140d30c9b0fbd041f35c67c57478cc705b28a67
DatSung-Dev <109453487+DatSung@users.noreply.github.com>
2022-10-30 19:56:06 +0700

    update

--- Workshop_7/Ex_1.c
+++ Workshop_7/Ex_1.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <string.h>
+
+char arrOfNames[255];
+
+void arrInput();
 
 int main() {
 
+    int num_names, i;
+
     printf("Please enter number of name to enter");
-    scantf()
+    scanf("%d", &num_names);
+    
+    for ( i = 0; i < num_names; i++)
+    {
+        arrInput();
+    }
+}
+
+void arrInput() {
+
+    int i;
+
+    for (i = 0; i < 255; i++)
+    {   
+        scanf("%[^\n]", arrOfNames[i]);
+        
+        if (arrOfNames[i] == '\n')
+        {
+            break;
+        }
+    }
+    
+    
     
 }
