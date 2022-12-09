26e7e2c1833d8351dcaae95aa0c69465f9133996
DatSung-Dev <109453487+DatSung@users.noreply.github.com>
2022-12-09 23:24:11 +0700

    update

--- Slot_67/LOOP WHILE AND AND DO WHILE/EX3_exponential.c
+++ Slot_67/LOOP WHILE AND AND DO WHILE/EX3_exponential.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
 
     int key_control, number_A, exponent_number, exponent_number_clone;
     double exponential_result = 1;
@@ -18,13 +19,12 @@ int main() {
         scanf("%d", &key_control);
 
         if (key_control == 1)
-        {   
+        {
             while (exponent_number <= exponent_number_clone && exponent_number >= 1)
             {
                 exponential_result *= number_A;
                 exponent_number -= 1;
             }
-        
         }
 
         if (key_control == 2)
@@ -34,7 +34,6 @@ int main() {
                 exponential_result *= number_A;
                 exponent_number -= 1;
             } while (exponent_number <= exponent_number_clone && exponent_number >= 1);
-        
         }
     }
 
@@ -43,25 +42,26 @@ int main() {
         if (number_A > 0)
         {
             exponential_result = 1;
-        } else if (number_A < 0)
+        }
+        else if (number_A < 0)
         {
             exponential_result = -1;
-        } else
+        }
+        else
         {
             printf("Indeterminate\n");
         }
-        
     }
 
     if (exponent_number_clone < 0)
-    {   
+    {
         exponent_number = -exponent_number;
         exponent_number_clone = exponent_number;
         printf("Press (1) to use While, press (2) to use Do While: ");
         scanf("%d", &key_control);
 
         if (key_control == 1)
-        {   
+        {
             while (exponent_number <= exponent_number_clone && exponent_number >= 1)
             {
                 exponential_result *= number_A;
@@ -76,12 +76,10 @@ int main() {
                 exponential_result *= number_A;
                 exponent_number -= 1;
             } while (exponent_number <= exponent_number_clone && exponent_number >= 1);
-        
         }
 
         exponential_result = 1 / exponential_result;
     }
-        
-    printf("The result of is: %lf\n", exponential_result);
 
+    printf("The result of is: %lf\n", exponential_result);
 }
--- Workshop_7/test.c
+++ Workshop_7/test.c
@@ -1,8 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h> 
-#include <string.h>
-int main() {
+#include<stdio.h>
+#include<math.h>
+void menu();
+int countelments();
+int sumelments();
+int invertarray();
 
-    char a() = "abcdefg";
-    printf(a()*10);
+int main()
+{
+   int choice;
+   int mark;
+   do
+   {
+   	printf("\n Choses a number:");
+   	menu();
+   	printf("\n Number enterd:");
+   	scanf("%d",&choice);
+   	switch(choice)
+   	{
+	 case 1:
+	 printf("Enter the number:");
+	 countelments();
+	 break;
+	}
+   }
+   while(choice!=0);
+   return 0;
+}
+void menu()
+{
+    printf("------------------------------------------------------------------------------------------------------------\n");
+	printf("\n1 Count elements that are primes in an array");	
+	printf("\n2 Sum the elements that are primes in the array");
+	printf("\n3 Invert array");
+	printf("\n4 Searching for an element in an array using the linear search algorithm");
+	printf("\n5 Search for an element in an array using binary search algorithm");
+	printf("\n6 Sort the array in descending order using the Selection sort algorithm");
+	printf("\n7 Sort the array in descending order using the Bubble sort algorithm");
+	printf("\n8 Add 1 element to array");
+	printf("\n9 Delete an element in an array");
+	
+}
+int countelments()
+{
+	int i, n;
+	int a[1000];
+	int count;
+    scanf("%d", &n);
+	for(i=0;i<n;i++)
+	{
+	   scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]%2 == 1)
+		{
+		  count++;
+		}	
+	}
+	printf("\nThe digit of odd numbers is: %d ",count);
+return 0;
 }
